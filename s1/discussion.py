5b2a35289cccb53b1f26368bfb67a943b7c40040
rapadajerico98 <rapadajericoavila98@gmail.com>
2025-08-29 09:28:48 +0000

    if else

--- s1/discussion.py
+++ s1/discussion.py
@@ -54,8 +54,59 @@ legal_age = 18
 
 my_age = int(input("Enter your age: "))
 
-if my_age >= legal_age:
-    print("You are now on a legal age. ")
+if my_age >= 60:
+    print("You are now on a senior citizen. ")
+elif my_age >= legal_age:
+    print("You are a now in a legal age")
 else: 
     print("You are not 18 year's old. ")
+
+edad = int(input("Ano ang iyong edad nigga?"))
+
+#Pag 18 pwede na uminom ng alak 
+#Pag 21 naman pede na mag drive at uminom ng alak
+#kapag below 18 bawal uminom ng alak at mag drive
+
+if edad >= 21:
+    print("Pwede na mag drive at uminom ng alak")
+elif edad >= 18:
+    print("pwede ka ng uminom ng alak")
+elif edad <= 17:
+    print("kapag below 18 bawal uminom ng alak at mag drive")
+
+
+grade = int(input("Enter your grade:"))
+
+#pag yung grade is above 90 to 100 is excellent
+#pag yung grade is 80 to 89 is very good
+#pag 75 to 79 is ok 
+#pag below 75 bagsak kang kupal ka1
+
+if grade >= 90 and grade <= 100:
+    print("Excellent")
+elif grade >= 80 and grade <= 89:
+    print("VeryGood")
+elif grade >= 75 and grade <= 79:
+    print("ok na pwede na")
+elif grade > 50 and grade <= 74:
+    print("Bagsak kang kupal ka!")
+else:
+    print("Invalid grade")
+
+#kapag ang pera mo ay  50_000 pataas mayaman 
+#kapag ang pera mo ay 30_000 to 49_000 sakto lang
+#kapag ang pera mo ay 20_000 to 29_000 mahirap kang kupal ka
+#else hampas lupa ka
+
+
+savings_account = int(input("Enter your savings money: "))
+if savings_account >= 50_000:
+    print("Mayaman ka!")
+elif savings_account >= 30_000 and savings_account <= 49_000:
+    print("sakto lang")
+elif savings_account >= 20_000 and savings_account <= 29_000:
+    print("mahirap kang kupal ka!")
+elif savings_account > 0 and savings_account <= 19_000:
+    print("wala na finish na ")
+
 
