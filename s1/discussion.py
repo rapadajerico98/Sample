505c0d9c920d649e7b254a02a2380431d998314c
rapadajerico98 <rapadajericoavila98@gmail.com>
2025-08-29 08:18:38 +0000

    discussion

---
+++ s1/discussion.py
@@ -0,0 +1,61 @@
+street_address = "Blk 4 Lot 5 Kapanalig st"
+barangay = "28"
+city = "Caloocan City"
+postal_code = "1104"
+country = "philippines"
+
+#print(f"i live at {street_address} my barangay is {barangay}, {city}, {postal_code}, {country}")
+
+a = 5
+b = 5
+c = a + b 
+
+#print(c)
+
+d = c * a
+
+#multipy
+
+data1 = 10 * 7
+print(data1)
+
+#division
+
+data2 = 100 / 10
+print(data2)
+
+
+num1 = 2
+num2 = 5
+
+
+#print(f"the total sum is {total_sum}")
+
+#d
+
+
+#multiplication
+"""
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+product = num1 * num2
+
+print("the total product is", product )
+"""
+#division
+
+#num1 = int(input("Enter first number: "))
+#num2 = int(input("Enter second number: "))
+#quotient = num1 / num2
+
+#print("the total quotient is", quotient)
+
+legal_age = 18
+
+my_age = int(input("Enter your age: "))
+
+if my_age >= legal_age:
+    print("You are now on a legal age. ")
+else: 
+    print("You are not 18 year's old. ")
+
